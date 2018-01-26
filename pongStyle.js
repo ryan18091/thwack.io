07bae5f32e1eab0a46c0d2fafa8ec8e7405fd57a
Ryan Erickson <ryanerickson74@gmail.com>
2018-01-26 15:20:35 -0600

    changed startMove function

--- pongStyle.js
+++ pongStyle.js
@@ -7,37 +7,37 @@ var Field=document.getElementsByClassName('Field');
 var leftPaddleTop = 76;
 var rightPaddleTop = 76;
 
-// listen any touch event
-document.addEventListener('touchstart', handleTouchEvent, true);
-document.addEventListener('touchmove', handleTouchEvent, true);
-document.addEventListener('touchend', handleTouchEvent, true);
-document.addEventListener('touchcancel', handleTouchEvent, true);
+// // listen any touch event
+// document.addEventListener('touchstart', handleTouchEvent, true);
+// document.addEventListener('touchmove', handleTouchEvent, true);
+// document.addEventListener('touchend', handleTouchEvent, true);
+// document.addEventListener('touchcancel', handleTouchEvent, true);
+//
+// // will adjust leftpaddl's y to latest touch
+// function handleTouchEvent(e) {
+//     if (e.touches.length === 0 ) return;
+//     e.preventDefault();
+//     e.stopPropagation();
+//     startMove();
+//     var touch = e.touches[0];
+//     if (e.pageY < 49) {
+//         return;
+//     } else if (e.pageY > 350) {
+//         return;
+//     }
+//     else {
+//         leftPaddle.style.top = (e.pageY - 50) + 'px';
+//     }
+// }
 
-// will adjust leftpaddl's y to latest touch
-function handleTouchEvent(e) {
-    if (e.touches.length === 0 ) return;
-    e.preventDefault();
-    e.stopPropagation();
-    startMove()
-    var touch = e.touches[0];
-    if (e.pageY < 49) {
-        return;
-    } else if (e.pageY > 350) {
-        return;
-    }
-    else {
-        leftPaddle.style.top = (e.pageY - 50) + 'px';
-    }
-}
 
-
-// will adjust leftpaddle's y to latest mouse event
-
-document.addEventListener('mousemove', handleMouseEvent);
-
-function handleMouseEvent(e) {
-    leftPaddle.style.top = (e.pageY - 50) + 'px';
-}
+// // will adjust leftpaddle's y to latest mouse event
+//
+// document.addEventListener('mousemove', handleMouseEvent);
+//
+// function handleMouseEvent(e) {
+//     leftPaddle.style.top = (e.pageY - 50) + 'px';
+// }
 
 var change = 0;
 var pressed = false;
@@ -142,6 +142,7 @@ function inBounds() {
 
 var angle = 1
 function startMove() {
+  if (posTop == 195 && posLeft == 295) {
   console.log('start');
   var ball = document.getElementById("ball");
   var id = setInterval(frame, 1);
@@ -151,8 +152,9 @@ function startMove() {
   var speed = 1
   function frame() {
       // console.log('frame');
-      getPosition();
+          getPosition();
       inBounds();
+
       //console.log(leftPaddleTop);
       function moveRight() {
           // Detects colision with right Paddle and starts moveLeft Functions
@@ -165,28 +167,28 @@ function startMove() {
                   angle = 0.5;
               } else if (strikeAngle >= 21 && strikeAngle <= 30) {
                   angle = 1.0;
-              } else if (strikeAngle >=31 && strikeAngle <= 40) {
+              } else if (strikeAngle >= 31 && strikeAngle <= 40) {
                   angle = 1.5;
               } else {
                   angle = 2.0;
               }
               moveLeft();
-          // Detects if ball has collided with far right line
-          } else if (posLeft >= 590){
+              // Detects if ball has collided with far right line
+          } else if (posLeft >= 590) {
               posLeft += speed;
               ball.style.left = posLeft + 'px';
               var player = 1
               score(player);
               reset();
               clearInterval(id);
-          // Detects if ball has collided with top line and switched movement to down
+              // Detects if ball has collided with top line and switched movement to down
           } else if (posTop <= 0) {
-                  posTop += angle;
-                  posLeft += speed;
-                  upDown = 'down';
-                  ball.style.left = posLeft + 'px';
-                  ball.style.top = posTop + 'px';
-          //Detects if ball has collided with bottom line and switched movement to up
+              posTop += angle;
+              posLeft += speed;
+              upDown = 'down';
+              ball.style.left = posLeft + 'px';
+              ball.style.top = posTop + 'px';
+              //Detects if ball has collided with bottom line and switched movement to up
           } else if (posTop >= 390) {
               posTop -= angle;
               posLeft += speed;
@@ -194,9 +196,9 @@ function startMove() {
               ball.style.left = posLeft + 'px';
               ball.style.top = posTop + 'px';
               //clearInterval(id);
-          //If ball has not collided with an object its movements is determined by the upDown var
+              //If ball has not collided with an object its movements is determined by the upDown var
           } else {
-              if (upDown == 'up'){
+              if (upDown == 'up') {
                   posTop -= angle;
                   posLeft += speed;
                   ball.style.left = posLeft + "px";
@@ -212,6 +214,7 @@ function startMove() {
 
           }
       }
+
       function moveLeft() {
           // Detects colision with left Paddle and starts moveRight Functions
           if (posLeft == 10 && leftPaddleTop <= posTop && (leftPaddleTop + 100) >= posTop) {
@@ -223,14 +226,14 @@ function startMove() {
                   angle = 0.5;
               } else if (strikeAngle >= 21 && strikeAngle <= 30) {
                   angle = 1.0;
-              } else if (strikeAngle >=31 && strikeAngle <= 40) {
+              } else if (strikeAngle >= 31 && strikeAngle <= 40) {
                   angle = 1.5;
               } else {
                   angle = 2.0;
               }
               moveRight();
-          // Detects if ball has collided with far left line
-          } else if (posLeft <=0){
+              // Detects if ball has collided with far left line
+          } else if (posLeft <= 0) {
               posLeft -= speed;
               ball.style.left = posLeft + 'px';
               var player = 2
@@ -239,15 +242,15 @@ function startMove() {
               clearInterval(id);
               // Detects if ball has collided with top line and switched movement to down
           } else if (posTop <= 0) {
-                  posTop += angle;
-                  posLeft -= speed;
-                  upDown = 'down';
-                  ball.style.left = posLeft + 'px';
-                  ball.style.top = posTop + 'px';
+              posTop += angle;
+              posLeft -= speed;
+              upDown = 'down';
+              ball.style.left = posLeft + 'px';
+              ball.style.top = posTop + 'px';
 
 
-                                //clearInterval(id);
-          //Detects if ball has collided with bottom line and switched movement to up
+              //clearInterval(id);
+              //Detects if ball has collided with bottom line and switched movement to up
           } else if (posTop >= 390) {
               posTop -= angle;
               posLeft -= speed;
@@ -256,16 +259,16 @@ function startMove() {
               ball.style.top = posTop + 'px';
 
               //clearInterval(id);
-          //If ball has not collided with an object its movements is determined by the upDown var
+              //If ball has not collided with an object its movements is determined by the upDown var
           } else {
-              if (upDown == 'up'){
+              if (upDown == 'up') {
                   posTop -= angle;
                   posLeft -= speed;
                   ball.style.left = posLeft + "px";
                   ball.style.top = posTop + 'px';
                   aI();
 
-                                //clearInterval(id);
+                  //clearInterval(id);
 
               } else {
                   posTop += angle;
@@ -274,13 +277,14 @@ function startMove() {
                   ball.style.top = posTop + "px";
                   aI();
 
-                                //clearInterval(id);
+                  //clearInterval(id);
 
               }
 
           }
 
       }
+
       //console.log(rightPaddleTop);
       if (move == 'right') {
           moveRight();
@@ -288,6 +292,9 @@ function startMove() {
           moveLeft();
       }
 
+    }
+  } else {
+      return;
   }
 }
 
@@ -302,8 +309,7 @@ var p2score = 0;
 document.body.onkeydown = function(e){
     if(e.keyCode == 32){
         startMove();
-        console.log(start);
-        // event.preventDefault()
+        event.preventDefault()
     }
 }
 
@@ -364,6 +370,7 @@ function handleTouchEvent(e) {
     e.preventDefault();
     e.stopPropagation();
     var touch = e.touches[0];
+    startMove();
     if (e.pageY < 49) {
         return;
     } else if (e.pageY > 350) {
@@ -373,5 +380,22 @@ function handleTouchEvent(e) {
         leftPaddle.style.top = (e.pageY - 50) + 'px';
     }
 }
+
+// if (startmove == 2) {
+//     startMove()
+// }
+
+// will adjust leftpaddle's y to latest mouse event
+
+document.addEventListener('mousemove', handleMouseEvent);
+
+function handleMouseEvent(e) {
+    leftPaddle.style.top = (e.pageY - 50) + 'px';
+    startMove();
+}
+
+
+
+
 
 });
